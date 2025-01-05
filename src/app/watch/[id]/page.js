7a4e2cd00e0ac0393f78cc19cfe6f731ab729ea1
Abubakar Amasa <amasaabubakar@gmail.com>
2025-01-05 18:44:15 +0000

    refactor-btn

--- src/app/watch/[id]/page.js
+++ src/app/watch/[id]/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { BASE_CONTENT_URL, BASE_STREAM_URL } from "@/app/values";
+import { BASE_CONTENT_URL, BASE_STREAM_URL, BASE_USER_PROFILE_URL } from "@/app/values";
 import axios from "axios";
 import { useEffect, useState } from "react";
 const {usePathname, useSearchParams} = require("next/navigation");
@@ -24,12 +24,26 @@ const Watch=()=>{
     useEffect(()=>{
         getVideoDetails()
     },[])
+    const addToLikes = async()=>{
+        try {
+            const cookies = document.cookie.split(';')
+            let parts = []
+            cookies.forEach(cookie=>{
+              if(cookie.includes('token')){
+                parts = cookie.split('=')
+              }})
+            const {data} = await axios.post(BASE_USER_PROFILE_URL + "/api/profile/like/", {id}, {headers: {token: `${parts[1]}`}})
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="aspect-w-16 aspect-h-9">
             {/* <div className="absolute top-10 left-0 w-full h-full flex  justify-center bg-black bg-opacity-30 text-white">
                 <h1 className="text-xl font-bold">Your Video Title</h1>
             </div> */}
-            <div className="h-[23vh] w-fit m-auto">
+            <div className="h-[30vh] w-fit m-auto">
                 <h1 className="text-xl w-fit m-auto font-bold">{videoDetails.title}</h1>
                 <div className="text-[5px]">
                     <span className="text-sm">Genre: </span>
@@ -37,8 +51,8 @@ const Watch=()=>{
                 </div>
                 <div className="text-sm w-fit m-auto">{videoDetails.releasedAt}</div>
                 <div className="flex items-center space-x-2">{videoDetails.description}</div>
-                <button className="bg-red-500 p-2 rounded-lg w-[50%] mt-[10px]">Like</button>
-                <button className="bg-red-500 ml-[10px] p-2 rounded-lg w-[50%] mt-[10px]">Add to watch list</button>
+                <button className="bg-red-500 p-2 rounded-lg w-fit mt-[10px]">Like</button>
+                <button className="bg-red-500 ml-[10px] p-2 rounded-lg w-fit mt-[10px]">Add to watch list</button>
             </div>
             <video poster={poster} controls className="w-full mt-[5vh] h-[500px]">
                 <source src={`${BASE_STREAM_URL}/video/stream/${id}`} type="video/mp4"/>
