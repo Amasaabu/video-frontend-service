5bb5e6f365d2fb9de347aea418a66833bedd7aff
Abubakar Amasa <amasaabubakar@gmail.com>
2025-08-17 22:24:10 +0100

    mod

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/user/:path*',
-                destination: 'http://auth-service/api/user/:path*', // Proxy to backend
-            },
-            {
-                source: '/video/:path*',
-                destination: 'http://stream-service/video/:path*', // Proxy to backend
-            },
-            {
-                source: '/api/content/:path*',
-                destination: 'http://upload-service/api/content/:path*', // Proxy to backend
-            },
-            {
-                source: '/api/profile/:path*',
-                destination: 'http://userutil-service/api/profile/:path*', // Proxy to backend
-            },
-        ];
-    },
-    output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/user/:path*",
+        destination: "http://auth-service/api/user/:path*", // Proxy to backend
+      },
+      {
+        source: "/video/:path*",
+        destination: "http://stream-service/video/:path*", // Proxy to backend
+      },
+      {
+        source: "/api/content/:path*",
+        destination: "http://upload-service/api/content/:path*", // Proxy to backend
+      },
+      {
+        source: "/api/profile/:path*",
+        destination: "http://userutil-service/api/profile/:path*", // Proxy to backend
+      },
+    ];
+  },
+  output: "standalone",
 };
-
+//
 export default nextConfig;
